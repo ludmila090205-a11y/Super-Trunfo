ae4c8af060b7a35cf985f40c3aa2519929874ca3
Ludmila Rodrigues <ludmila090205@gmail.com>
2025-09-10 21:27:26 -0300

    iniciar

---
+++ arquivo.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+int main() {
+    int num1, num2, soma;
+
+    // Lendo dois números do usuário
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
